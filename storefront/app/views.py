a83d8a29e4d9512cab3290c8118b22cc81d34b92
TheDukato <56132686+TheDukato@users.noreply.github.com>
2023-11-19 11:44:48 +0100

    First connection frontend and backend

--- storefront/app/views.py
+++ storefront/app/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 
 def say_sth(request):
-    return render(request, "first.html") #HttpResponse("hi")
+    return render(request, "index.html") #HttpResponse("hi")
