acbcc859563f248f1df17acf028270ba9c646f3e
MPtak <mptak@student.agh.edu.pl>
2023-11-25 18:47:20 +0100

    First db integration

--- storefront/app/views.py
+++ storefront/app/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Animal
 
 def main_page(request):
-    return render(request, "index.html")
+    #animal1 = Animal.objects.create(name='Kit', type='Cat')
+    #animal1.save()
+
+    all_animals = Animal.objects.all
+    return render(request, "index.html", {"all": all_animals})
 
 
 def second_page(request):
