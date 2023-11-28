72d0b64b7294a7eac5e9fbb61b78b8dbd1db5f64
MPtak <mptak@student.agh.edu.pl>
2023-11-28 20:21:42 +0100

    Add buttons WPŁAĆ, which updates tot cost of 20 zl

--- storefront/app/urls.py
+++ storefront/app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.main_page),
-    path('test_page/', views.second_page, name='test_page')
+    path('', views.main_page, name="main"),
+    path('test_page/', views.second_page, name='test_page'),
+    path('bump/<int:pk>', views.bump, name="bump"),
 ]
--- storefront/app/views.py
+++ storefront/app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Animal
+
 
 def main_page(request):
     #animal1 = Animal.objects.create(name='Kit', type='Cat')
@@ -10,5 +11,17 @@ def main_page(request):
     return render(request, "index.html", {"all": all_animals})
 
 
+def bump(request, pk):
+
+    a = Animal.objects.get(pk=pk)
+    print(f"Updated {a.name}")
+    a.donations_amount += 20
+    a.total_donations += 1
+    a.save()
+    return HttpResponseRedirect(request. META. get('HTTP_REFERER', '/'))
+
+
 def second_page(request):
     return HttpResponse("This is test page content")
+
+
