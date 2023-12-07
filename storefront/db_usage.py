1b266fe4caad8d9a44c0bf94e6a8962a5d46c367
Jatczyszyn <jatczyszyn@student.agh.edu.pl>
2023-12-07 22:54:53 +0100

    Add information about animals

--- storefront/db_usage.py
+++ storefront/db_usage.py
@@ -15,8 +15,10 @@ import datetime
 # Add one record to tables
 user = User.objects.create(name='John', surname='Kowalsky', email='jk@pdf.com', password='kolysanka', birth_date='2000-01-01') # Default date format: YYYY-MM-DD)
 
-animal1 = Animal.objects.create(name='Kit', type='Cat')
-animal2 = Animal.objects.create(name='Leszek', type='Dog')
+animal1 = Animal.objects.create(name='Kit', type='Cat', description='Kotek Kit ma cztery łapki i słodki nosek. Został podrzucony do schroniska z złamaną łapką. Potrzeba 100zł na operację.', picture='images/1620728230.jpg')
+animal2 = Animal.objects.create(name='Leszek', type='Dog', description='Leszek to wspaniały piesek rasy pudel. Szczeka na zawołanie i podaje łapke. Jednak w wyniku nieszczęśliwej choroby Leszek potrzebuje przeszczepu nerki, aby dokonać takiej operacji musimy zebrać 500zł.', picture='images/pobrane.jpeg')
+animal3 = Animal.objects.create(name='Taco', type='Tiger', description='Tygrysek Taco jest w schronisku od dwóch lat. Potrzeuje bardzo szybkiej operacji uzębienia. Aby dokonać skomplikowanej wymiany zęba potzebuje 300 zł, dlatego prosi Was o pomoc', picture='images/cute-baby-animal-pictures-zzdaek1h4l810z9w.jpg')
+animal4 = Animal.objects.create(name='Pegas', type='Horse', description='Pegas to nasz najlepszy koń, tworzy niesamowitą więź ze swoimi jeźdzcami. Żeby nadal mógł cieszyć się z galopowania, musi przebyć operacje kopytka. Potrzebuje 400 zł', picture='images/6b668d979f0b144e77697b5340435aea80017de34fc93536c5674b573c1db7b2d5233e853459f21331d929e0f79eb19b304c72bb5562dc9f15087c_1280.jpg')
 
 # datetime.date
 d = datetime.date(2024, 6, 27)
@@ -32,10 +34,14 @@ for t in transfer_types:
           
 donation = Donation.objects.create(user_id=user, animal_id=animal1, type_id=TransferType.objects.get(type="przelew tradycyjny"), amount=50.50)
 donation2 = Donation.objects.create(user_id=user, animal_id=animal2, type_id=TransferType.objects.get(type='barter'), amount=607.06)
+donation3 = Donation.objects.create(user_id=user, animal_id=animal3, type_id=TransferType.objects.get(type='paysafecard'), amount=100.45)
+donation4 = Donation.objects.create(user_id=user, animal_id=animal4, type_id=TransferType.objects.get(type='barter'), amount=300)
 
 # Modify records
 donations_to_update_for_animal1 = Donation.objects.filter(animal_id=animal1.id)
 donations_to_update_for_animal2 = Donation.objects.filter(animal_id=animal2.id)
+donations_to_update_for_animal3 = Donation.objects.filter(animal_id=animal3.id)
+donations_to_update_for_animal4 = Donation.objects.filter(animal_id=animal4.id)
 
 for d in donations_to_update_for_animal1:
     animal1.donation_target = 100.00
@@ -47,8 +53,20 @@ for d in donations_to_update_for_animal2:
     animal2.donations_amount = Decimal(animal2.donations_amount) + d.amount
     animal2.total_donations += 1
 
+for d in donations_to_update_for_animal3:
+    animal3.donation_target = 300.00
+    animal3.donations_amount = Decimal(animal3.donations_amount) + d.amount
+    animal3.total_donations += 1
+
+for d in donations_to_update_for_animal4:
+    animal4.donation_target = 400.00
+    animal4.donations_amount = Decimal(animal4.donations_amount) + d.amount
+    animal4.total_donations += 1
+
 animal1.save()
 animal2.save()
+animal3.save()
+animal4.save()
 
 # Print tables
 users = User.objects.all()
@@ -62,7 +80,7 @@ for user in users:
 
 print("\nAnimal:")
 for animal in animals:
-    print(f"ID: {animal.id}: {animal}, Funds collected: {animal.donations_amount}/{animal.donation_target}, Total donations: {animal.total_donations}, Due date: {animal.due_date}")
+    print(f"ID: {animal.id}: NAME: {animal.name}, TYPE: {animal.type}, DESCRIPTION: {animal.description}, PICTURE: {animal.picture}, Funds collected: {animal.donations_amount}/{animal.donation_target}, Total donations: {animal.total_donations}, Due date: {animal.due_date}")
 
 print("\nDonation:")
 for donation in donations:
@@ -75,7 +93,7 @@ users.delete()
 donations.delete()
 
 # Print tables
-donations = Donation.objects.all() 
+donations = Donation.objects.all()
 users = User.objects.all()
 animals = Animal.objects.all()
 transfer_types = TransferType.objects.all()
