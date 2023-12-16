feaa365fdc0963eafb8e7793b3b244246dac1c36
Bartosz Kowal <bkowal@student.agh.edu.pl>
2023-12-16 13:22:30 +0100

    Dodanie funkcji przełączania stron - nieskończone

--- storefront/app/views.py
+++ storefront/app/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Animal
 from django.views.decorators.csrf import csrf_exempt
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def main_page(request):
@@ -42,7 +43,7 @@ def dotation_page(request):
 
 def about_us(request):
     return render(request, "O-nas.html")
-
+\
 
 def auctions(request):
     return render(request, "Licytacje.html")
@@ -50,3 +51,28 @@ def auctions(request):
 
 def login(request):
     return render(request, "Login.html")
+
+def auctions(request):
+    # Pobierz wszystkie licytacje
+    all_auctions = Animal.objects.all()
+
+    # Określ liczbę licytacji na stronie
+    auctions_per_page = 2
+
+    # Utwórz obiekt paginacji
+    paginator = Paginator(all_auctions, auctions_per_page)
+
+    # Pobierz numer strony z parametru GET (jeśli nie ma, przyjmij 1)
+    page = request.GET.get('page', 1)
+
+    try:
+        # Pobierz obiekt Page dla danej strony
+        auctions_page = paginator.page(page)
+    except PageNotAnInteger:
+        # Jeśli numer strony nie jest liczbą całkowitą, pobierz pierwszą stronę
+        auctions_page = paginator.page(1)
+    except EmptyPage:
+        # Jeśli numer strony jest poza zakresem, pobierz ostatnią stronę
+        auctions_page = paginator.page(paginator.num_pages)
+
+    return render(request, "Licytacje.html", {"auctions_page": auctions_page})
